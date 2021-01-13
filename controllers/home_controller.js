60baf1ebb58c6409db94f5d85c35eecb4894cbf4
azhussainn <azhuss77@gmail.com>
2021-01-13 08:15:53 +0530

    added user controller and user route

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    return res.end("<h1>Express is up for Codeial</h1>")
+    return res.end("<h1>Express is up for Codeial</h1>");
+}
+module.exports.test = function(req, res){
+    return res.end("<h1>The test is successful</h1>");
 }
