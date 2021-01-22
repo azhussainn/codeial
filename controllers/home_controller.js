b5bfd2bce426049bef44dc4587c2f8919209efa5
azhussainn <azhuss77@gmail.com>
2021-01-22 19:24:03 +0530

    added delete post functionality

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,18 @@
 const Post = require('../models/post');
 module.exports.home = function(req, res){
-    
-    //Populate the user of each post
+
+    //find posts details (i.e post content)
     Post.find({})
+
+    //get users details from the posts (i.e post user name)
     .populate('user')
+
     .populate({
+        //get comments from the posts (i.e post comments)
         path : "comments",
+
         populate : {
+            //get user details from the comments i.e (post comment user name)
             path: "user"
         }
     })
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = function(req, res){
     if(req.body.content != ""){
         Post.create({
@@ -14,4 +15,25 @@ module.exports.create = function(req, res){
     else{
         return res.redirect('back');
     }
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err){console.log('error in deleting the post'); return}
+        else{
+            //.id means converting thr object id into string
+            if(post.user == req.user.id){
+                post.remove();
+                Comment.deleteMany({post : req.params.id}, function(err){
+                    if(err){console.log('error in deleting the comments'); return}
+                    else{
+                        return res.redirect('back');
+                    }
+                })
+            }
+            else{
+                return res.redirect('back');
+            }
+        }
+    })
 }
