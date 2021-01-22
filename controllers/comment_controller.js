42dbf8dfbb29582a327b441b40217f3f1b4cb849
azhussainn <azhuss77@gmail.com>
2021-01-22 18:36:23 +0530

    addded comments to views

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -3,23 +3,29 @@ const Comment = require('../models/comment');
 
 
 module.exports.create = function(req, res){
-    Post.findById(req.body.post, function(err, post){
-        if(post){
-            Comment.create({
-                content : req.body.content,
-                user : req.user._id,
-                post : req.body.post
-            }, function(err, comment){
-                if(err){ console.log('error in creating a comment'); return }
-                else{
-                    post.comments.push(comment);
-                    post.save();
-                    return res.redirect('back');
-                }
-            })
-        }
-        else{
-            return res.redirect('back');
-        }
-    })
+    if(req.body.content != ""){
+        Post.findById(req.body.post, function(err, post){
+            if(post){
+                Comment.create({
+                    content : req.body.content,
+                    user : req.user._id,
+                    post : req.body.post
+                }, function(err, comment){
+                    if(err){ console.log('error in creating a comment'); return }
+                    else{
+                        post.comments.push(comment);
+                        post.save();
+                        return res.redirect('back');
+                    }
+                })
+            }
+            else{
+                return res.redirect('back');
+            }
+        })
+    }
+    else{
+        return res.redirect('back');
+    }
+
 }
