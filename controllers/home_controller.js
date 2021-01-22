2b7b07679b1df4c01948d42c4726a9c3282f2baf
azhussainn <azhuss77@gmail.com>
2021-01-22 11:53:11 +0530

    added css to home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,8 +3,8 @@ module.exports.home = function(req, res){
     
     //Populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
-        return res.render('home.ejs', {
-            title : "Home",
+        return res.render('home', {
+            title : "Codeial | Home",
             posts : posts
         });
     })
