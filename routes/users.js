b93155a8224810cff5ef0dc7ea4decf17f6254f0
azhussainn <azhuss77@gmail.com>
2021-01-23 20:09:43 +0530

    added profile edit functionality

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,12 @@ const passport = require('passport');
 
 const usersController = require('../controllers/users-controller');
 
-router.get('/profile',passport.checkAuthentication, usersController.profile);
+router.get('/profile/:id',passport.checkAuthentication, usersController.profile);
+router.post('/update/:id',passport.checkAuthentication, usersController.update);
+
 router.get('/sign-in', usersController.signIn);
 router.get('/sign-up', usersController.signUp);
+
 router.post('/create', usersController.create);
 router.get('/sign-out', usersController.destroySession);
 
